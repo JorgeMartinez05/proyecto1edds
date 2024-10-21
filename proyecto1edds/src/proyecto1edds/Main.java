60b53c24f82b7d4799b10896781473d459a73844
enriqueleonn <ealu2005@gmail.com>
2024-10-20 20:47:39 -0400

    Clase Estacion

--- proyecto1edds/src/proyecto1edds/Main.java
+++ proyecto1edds/src/proyecto1edds/Main.java
@@ -14,7 +14,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Estacion estacion = new Estacion("Los Dos Caminos");
+        Estacion estacion2 = new Estacion("Parque del Este");
+        Estacion estacion3 = new Estacion("Altamira");
+        Estacion estacion4 = new Estacion("Chacao");
+        System.out.println(estacion);
     }
     
 }
