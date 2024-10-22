57ceda808950f423a64551a5928de8b8e1dc7bfc
enriqueleonn <ealu2005@gmail.com>
2024-10-21 21:15:23 -0400

    Update Cargar.java

--- proyecto1edds/src/Funcionalidades/Cargar.java
+++ proyecto1edds/src/Funcionalidades/Cargar.java
@@ -9,5 +9,50 @@ package Funcionalidades;
  * @author eabdf
  */
 public class Cargar {
+    private Lista vertices = new Lista();
+    
+    public Lista getVertices() {
+        return vertices;
+    }
+    
+    public void setVertices(Lista vertices) {
+        this.vertices = vertices;
+    }
+    
+    public void cargarArchivoJSON(String rutaArchivo) {
+        try {
+            Gson gson = new Gson();
+            JsonObject redTransporteData = gson.fromJson(new FileReader(rutaArchivo), JsonObject.class);
+            
+            
+            Lista nombresRedes = obtenerClavesDeJsonObject (redTransporteData);
+            
+            
+            for (int i = 0; i< nombresRedes.getSize(); i++) {
+                String nombreRed = (String) nombresRedes.getValor(i);
+                JsonElement redElement = redTransporteData.get(nombreRed);
+                
+                
+                if (redElement.isJsonObject()) {
+                    JsonObject lineasObject = redElement.getAsJsonObject();
+                    procesarLineasRed (lineasObject);
+                }
+                else if (redElement.isJsonArray()) {
+                    JsonArray lineasArray = redElement.getAsJsonArray();
+                    
+                    for(JsonElement elementoLinea : lineasArray) {
+                        if (elementoLinea.isJsonObject()) {
+                            JsonObject lineaObject = elementoLinea.getAsJsonObject();
+                            procesarLineasRed(lineaObject);
+                        }
+                    }
+                }
+            }
+            
+        }catch (IOExcepcion e) {
+            e.printStackTrace ();
+        }
+    }
+    
     
 }
