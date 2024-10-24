6fcee10bf924d21d483ed56f1795e53eaf2c8066
KikeBejarano <eabdf92@gmail.com>
2024-10-24 12:31:19 -0400

    Update interfaz CargaDeRed

--- proyecto1edds/src/GUI/CargaDeRed.java
+++ proyecto1edds/src/GUI/CargaDeRed.java
@@ -4,17 +4,31 @@
  */
 package GUI;
 
+import EDD.Grafo;
+import EDD.Vertice;
+import Funcionalidades.Cargar;
+import Funcionalidades.JSONFileSelector;
+import Funcionalidades.Validacion;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author eabdf
  */
 public class CargaDeRed extends javax.swing.JFrame {
 
+    public static Grafo grafoEDD = new Grafo();
+    public static int t;
+
     /**
      * Creates new form CargaDeRed
      */
     public CargaDeRed() {
         initComponents();
+        this.setVisible(true);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+
     }
 
     /**
@@ -26,21 +40,109 @@ public class CargaDeRed extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jButton2 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextPane1 = new javax.swing.JTextPane();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        cargar = new javax.swing.JButton();
+        volver = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jButton2.setText("jButton2");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Arial Black", 0, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 255));
+        jLabel1.setText("CARGAR RED DE TRANSPORTE ");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 20, -1, -1));
+
+        jScrollPane1.setViewportView(jTextPane1);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 60, 320, -1));
+
+        jButton1.setFont(new java.awt.Font("Arial Black", 0, 14)); // NOI18N
+        jButton1.setText("Archivo");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 60, -1, -1));
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane2.setViewportView(jTextArea1);
+
+        jPanel1.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 100, 400, 270));
+
+        cargar.setFont(new java.awt.Font("Arial Black", 0, 14)); // NOI18N
+        cargar.setText("Cargar Red");
+        cargar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cargarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(cargar, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 380, -1, -1));
+
+        volver.setFont(new java.awt.Font("Arial Black", 0, 14)); // NOI18N
+        volver.setText("Volver");
+        volver.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                volverActionPerformed(evt);
+            }
+        });
+        jPanel1.add(volver, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 380, -1, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 560, 420));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        JSONFileSelector selector = new JSONFileSelector();
+        selector.buscarArchivoJson(jTextPane1, jTextArea1, this);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void cargarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cargarActionPerformed
+        Cargar cargarRedTransporte = new Cargar();
+        cargarRedTransporte.cargarArchivoJSON(jTextPane1.getText());
+
+        for (int i = 0; i < cargarRedTransporte.getVertices().getSize(); i++) {
+            Vertice verticeActual = (Vertice) cargarRedTransporte.getVertices().getValor(i);
+            grafoEDD.InsertarVertice(verticeActual);
+        }
+
+        grafoEDD.mostrar();
+
+        Validacion validacion = new Validacion();
+
+        String valorT = "";
+        valorT = JOptionPane.showInputDialog("Valor inicial de T: ");
+        while (validacion.ValidarNumero(valorT) == -1) {
+            valorT = JOptionPane.showInputDialog("Valor inicial de T:");
+        }
+
+        int valorInicialT = validacion.ValidarNumero(valorT);
+        t = valorInicialT;
+
+        MenuPrincipal menu = new MenuPrincipal();
+        this.dispose();
+
+
+    }//GEN-LAST:event_cargarActionPerformed
+
+    private void volverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_volverActionPerformed
+        Bienvenida ventana = new Bienvenida();
+        grafoEDD.destruir();
+        this.dispose();
+    }//GEN-LAST:event_volverActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +180,15 @@ public class CargaDeRed extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cargar;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextPane jTextPane1;
+    private javax.swing.JButton volver;
     // End of variables declaration//GEN-END:variables
 }
