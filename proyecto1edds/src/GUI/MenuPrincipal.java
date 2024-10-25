f50aca16a4197ea0d45b5a8f3403b44a373ebf00
JorgeMartinez05 <jorge2005.martinez@gmail.com>
2024-10-25 13:16:48 -0400

    esteticamenu

--- proyecto1edds/src/GUI/MenuPrincipal.java
+++ proyecto1edds/src/GUI/MenuPrincipal.java
@@ -15,6 +15,9 @@ public class MenuPrincipal extends javax.swing.JFrame {
      */
     public MenuPrincipal() {
         initComponents();
+        this.setVisible(true);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -34,14 +37,18 @@ public class MenuPrincipal extends javax.swing.JFrame {
         EstablecerValorT = new javax.swing.JButton();
         mostrarRed = new javax.swing.JButton();
         CargarRed = new javax.swing.JButton();
-        SalirMenu = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jPanel1.setBackground(new java.awt.Color(102, 102, 102));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Arial Black", 0, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Menu Principal");
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 20, -1, -1));
 
@@ -73,15 +80,26 @@ public class MenuPrincipal extends javax.swing.JFrame {
         });
         jPanel1.add(CargarRed, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 170, 120, -1));
 
-        SalirMenu.setText("X");
-        SalirMenu.addActionListener(new java.awt.event.ActionListener() {
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, 580, 290));
+
+        jMenu1.setText("inicio");
+        jMenu1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                SalirMenuActionPerformed(evt);
+                jMenu1ActionPerformed(evt);
             }
         });
-        jPanel1.add(SalirMenu, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 10, 60, 50));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, 580, 290));
+        jMenuItem1.setText("Cerrar");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -94,9 +112,12 @@ public class MenuPrincipal extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_CargarRedActionPerformed
 
-    private void SalirMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SalirMenuActionPerformed
-       System.exit(0);
-    }//GEN-LAST:event_SalirMenuActionPerformed
+    private void jMenu1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenu1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenu1ActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+System.exit(0);    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -138,10 +159,12 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JButton CargarRed;
     private javax.swing.JButton Coberturas;
     private javax.swing.JButton EstablecerValorT;
-    private javax.swing.JButton SalirMenu;
     private javax.swing.JButton colocarSucursal;
     private javax.swing.JButton eliminarSucursal;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton mostrarRed;
     // End of variables declaration//GEN-END:variables
